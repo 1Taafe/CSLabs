657bf72c03d9ac9b8a12157eb43fb8b3d22226c7
Dmitry Zayankovsky <90055752+1Taafe@users.noreply.github.com>
2021-12-24 13:01:52 +0300

    Add files via upload

---
+++ lab15/Numbers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace lab15
+{
+    public static class Numbers
+    {
+
+        static Random rnd = new Random();
+        static int value;
+        public static void Simple(object n)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\dima7\source\repos\lab15\nums.txt", false, System.Text.Encoding.Default))
+            {
+                for (int i = 2; i < (int)n; i++)
+                    for (int j = 2; j * j <= i; j++)
+                    {
+                        if (i % j == 0)
+                            break;
+                        else if (j + 1 > Math.Sqrt(i))
+                        {
+                            Console.WriteLine(i);
+                            Thread.Sleep(100);
+                            sw.WriteLine(i);
+                        }
+
+                    }
+            }
+        }
+
+        public static void Even(object n)
+        {
+            for(int i = 1; i <= (int)n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Thread.Sleep(100);
+                    Console.WriteLine(i);
+                    Write(i);
+                }
+            }
+        }
+
+        public static void Odd(object n)
+        {
+            for (int i = 1; i <= (int)n; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Thread.Sleep(300);
+                    Console.WriteLine(i);
+                    Write(i);
+                }
+            }
+        }
+
+        static void Write(int o)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\dima7\source\repos\lab15\evenANDodd.txt", true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(o);
+            }
+        }
+
+        public static void GetRandomNumber(object o)
+        {
+            value = rnd.Next(0, 100);
+            Console.WriteLine($"New RANDOM VALUE GENERATED : {value}");
+        }
+    }
+}
---
+++ lab15/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace lab15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\dima7\source\repos\lab15\processes.txt", false, System.Text.Encoding.Default))
+            {
+                var currentProcesses = Process.GetProcesses();
+                foreach (var s in currentProcesses)
+                {
+                    sw.WriteLine($"ID: {s.Id} | Name: {s.ProcessName} | BasePriority: {s.BasePriority}");
+                }
+            }
+
+            //2
+            AppDomain myDomain = AppDomain.CurrentDomain;
+            Console.WriteLine(myDomain.FriendlyName);
+            var setupInfo = myDomain.SetupInformation;
+            Console.WriteLine(setupInfo.ApplicationBase + " | " + setupInfo.TargetFrameworkName);
+            var builds = myDomain.GetAssemblies();
+            foreach (var s in builds)
+            {
+                Console.WriteLine(s);
+            }
+
+            //Isnt supported
+            //AppDomain newDomain = AppDomain.CreateDomain("new");
+            //newDomain.Load("AssemblyName");
+            //builds = newDomain.GetAssemblies();
+            //foreach (var s in builds)
+            //{
+            //    Console.WriteLine(s);
+            //}
+            // AppDomain.Unload(newDomain);
+
+
+            //3
+            Thread myThread = new Thread(new ParameterizedThreadStart(Numbers.Simple));
+            myThread.Start(30);
+            //myThread.Suspend(); 
+            //myThread.Resume();
+            Console.WriteLine($"{myThread.Name} | {myThread.Priority} | {myThread.ManagedThreadId}");
+
+            Thread.Sleep(4000);
+
+            //4
+            Thread even = new Thread(new ParameterizedThreadStart(Numbers.Even));
+            Thread odd = new Thread(new ParameterizedThreadStart(Numbers.Odd));
+
+            even.Priority = ThreadPriority.AboveNormal;
+            odd.Priority = ThreadPriority.BelowNormal;
+
+            even.Start(10);
+            odd.Start(10);
+
+            //5
+            TimerCallback tm = new TimerCallback(Numbers.GetRandomNumber);
+            Timer timer = new Timer(tm, null, 0, 3000);
+
+            Console.ReadLine();
+        }
+    }
+}
